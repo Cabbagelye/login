d47de7da8cdd8537c7a9461a67f055acaa4db72a
cabbagelye <cabbagelyeWork@163.com>
2022-11-26 16:41:14 +0800

    调整用户注册接口

--- src/main/java/com/cabbagelye/login/simple/dao/TestLoginDao.java
+++ src/main/java/com/cabbagelye/login/simple/dao/TestLoginDao.java
@@ -3,8 +3,15 @@ package com.cabbagelye.login.simple.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cabbagelye.login.model.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestLoginDao extends BaseMapper<User> {
 
 
+    /**
+     * 获取当天最后注册的序号
+     * @param today
+     * @return
+     */
+    String findMaxSerialNumByDay(@Param("today") String today);
 }
