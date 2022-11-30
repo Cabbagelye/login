31a94d7c295847814f93a941d78503133a5ab9dc
cabbagelye <cabbagelyeWork@163.com>
2022-11-30 10:42:29 +0800

    整合nacos

--- src/main/java/com/cabbagelye/login/oauth/dao/OAuthDao.java
+++
@@ -1,9 +0,0 @@
-package com.cabbagelye.login.oauth.dao;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cabbagelye.login.model.User;
-
-public interface OAuthDao extends BaseMapper<User> {
-
-}
