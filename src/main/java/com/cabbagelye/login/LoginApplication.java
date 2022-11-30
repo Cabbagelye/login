2509ad36604115a0e22b583a14eff63385bc4d10
cabbagelye <cabbagelyeWork@163.com>
2022-11-30 10:41:41 +0800

    整合nacos

--- src/main/java/com/cabbagelye/login/LoginApplication.java
+++ src/main/java/com/cabbagelye/login/LoginApplication.java
@@ -3,9 +3,11 @@ package com.cabbagelye.login;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan({"com.cabbagelye.login"})
+@EnableDiscoveryClient
 public class LoginApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cabbagelye/login/config/DruidConfiguration.java
@@ -0,0 +1,67 @@
+package com.cabbagelye.login.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@RefreshScope
+public class DruidConfiguration{
+
+    @Value("${spring.datasource.druid.url}")
+    private String url;
+    @Value("${spring.datasource.druid.username}")
+    private String username;
+    @Value("${spring.datasource.druid.password}")
+    private String password;
+    @Value("${spring.datasource.druid.driver-class-name}")
+    private String driverClassName;
+
+    @Bean(name = "dataSource")
+    @RefreshScope
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+}
