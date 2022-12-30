748e48ab0306240b9c2634b26fcb8df07761bc5d
git <123@163.com>
2022-12-30 18:01:17 +0800

    整合kafka

---
+++ src/main/java/com/cabbagelye/kafka/KafkaTopic.java
@@ -0,0 +1,6 @@
+package com.cabbagelye.kafka;
+
+public class KafkaTopic {
+
+    public static final String simpleTopic = "simpleTopic";
+}
--- src/main/java/com/cabbagelye/login/simple/controller/TestLoginController.java
+++ src/main/java/com/cabbagelye/login/simple/controller/TestLoginController.java
@@ -1,8 +1,12 @@
 package com.cabbagelye.login.simple.controller;
 
+import com.cabbagelye.kafka.KafkaTopic;
 import com.cabbagelye.login.model.po.User;
 import com.cabbagelye.login.model.dto.SignAccountDto;
 import com.cabbagelye.login.simple.service.TestLoginService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,5 +43,21 @@ public class TestLoginController {
             throw new RuntimeException("登录账号不能为空");
         }
         return testLoginService.getUserInfo(account);
+    }
+
+    @Resource
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    @PostMapping("/kafkaSendMsg")
+    @ResponseBody
+    public void sendMsg(){
+        //
+        kafkaTemplate.send(KafkaTopic.simpleTopic,"this is simple kafka.....");
+    }
+
+    @KafkaListener(topics = KafkaTopic.simpleTopic)
+    public void listenMsg(ConsumerRecord<?,String> record){
+        String value = record.value();
+        System.out.println("kafka value is :" + value);
     }
 }
